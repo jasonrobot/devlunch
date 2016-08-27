a02688a890a6a310d31e4a48d387d6a45e0714c2
Jason Howell <Jason Howell>
2016-08-26 18:31:22 -0700

    added views for the main page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,15 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
+  end
+
+  def lunch
+    polls_open
+  end
+
+  def polls_open
+  end
+
+  def polls_closed
   end
 end
