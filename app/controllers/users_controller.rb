ec74840149095f5d95d64ea2a37f4b074b5bd19b
Jason Howell <Jason Howell>
2016-08-26 18:57:04 -0700

    checkpoint before altering model

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,22 @@
 class UsersController < ApplicationController
   def new
-    @user = User.new
+  end
+
+  def create
+    @user = User.new(params[:user])
+    # if params['name'] == 'plusone' do
+    #
+    # else
+    #
+    # end
   end
 
   def lunch
+    @user = User.new
     polls_open
   end
+
+private
 
   def polls_open
     render 'polls_open.html.erb'
