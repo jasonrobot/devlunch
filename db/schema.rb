d6f7ad9b6221589905081984dcb8ae588ab7a1ce
Jason Howell <Jason Howell>
2016-08-27 12:42:45 -0700

    dropped devs table

--- db/schema.rb
+++ db/schema.rb
@@ -10,16 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160827185702) do
-
-  create_table "devs", force: :cascade do |t|
-    t.string   "name"
-    t.string   "hipchat_handle"
-    t.string   "pick"
-    t.datetime "created_at",     null: false
-    t.datetime "updated_at",     null: false
-    t.boolean  "plus_one"
-  end
+ActiveRecord::Schema.define(version: 20160827194032) do
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
