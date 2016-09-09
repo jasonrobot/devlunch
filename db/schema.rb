c825c79564bdb771ed08ecb7c533fea7e700dd6e
Jason Howell <jasonrobot64@gmail.com>
2016-09-08 21:41:59 -0700

    undid dropping the name column from users, made travis.yml actually run the tests

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160904065011) do
+ActiveRecord::Schema.define(version: 20160904051940) do
 
   create_table "picks", force: :cascade do |t|
     t.boolean  "done"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 20160904065011) do
     t.datetime "updated_at",                 null: false
     t.boolean  "plus_one",   default: false
     t.boolean  "coming",     default: true
+    t.string   "name"
   end
 
   create_table "winners", force: :cascade do |t|
